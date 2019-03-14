1859e76af082747665ae7d5ea3c63668202fa5e5
Tharunkumarcs <47142208+Tharunkumarcs@users.noreply.github.com>
2019-03-14 16:15:30 +0530

    Create hours.c

---
+++ hours.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int min,hrs,m,secs=00;
+	printf("enter the time in minutes:");
+	scanf("%d",&min);
+	hrs=min/60;
+	m=min%60;
+	if(m>60)
+	{
+		secs=m%60;
+		m=m-secs;
+	}
+	printf("%d %d %d",hrs,m,secs);
+	return 0;
+}

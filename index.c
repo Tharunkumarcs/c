86ff00fa2f014edd4908ef1624e5fe675a8f274c
Tharunkumarcs <47142208+Tharunkumarcs@users.noreply.github.com>
2019-03-14 16:10:06 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[100],i,n;
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        scanf("%d",&a[n]);
+        printf("%d \t %d\n",a[n],i);
+    }
+    
+}
